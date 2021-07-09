6d83eef06d64b2ec33876eec8903ac63c583178f
Evgeniy Stratonikov <evgeniy@nspcc.ru>
2021-07-09 16:59:38 +0300

    vm: add benchmark for reference counting
    Signed-off-by: Evgeniy Stratonikov <evgeniy@nspcc.ru>

--- pkg/vm/ref_counter_test.go
+++ pkg/vm/ref_counter_test.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/nspcc-dev/neo-go/pkg/vm/stackitem"
@@ -30,4 +31,56 @@ func TestRefCounter_Add(t *testing.T) {
 
 	r.Remove(arr)
 	require.Equal(t, 2, r.size)
+}
+
+func BenchmarkRefCounter(b *testing.B) {
+	b.Run("big array", func(b *testing.B) {
+		b.Run("new", func(b *testing.B) {
+			arr := stackitem.NewArray([]stackitem.Item{})
+			for i := 0; i < 2040; i++ {
+				arr = stackitem.NewArray([]stackitem.Item{arr})
+			}
+			v := New()
+			benchmarkRefCounter(b, v, arr)
+		})
+		b.Run("repeat", func(b *testing.B) {
+			arr := stackitem.NewArray([]stackitem.Item{})
+			for i := 0; i < 2040; i++ {
+				arr = stackitem.NewArray([]stackitem.Item{arr})
+			}
+			v := New()
+			v.estack.PushVal(arr)
+			benchmarkRefCounter(b, v, arr)
+		})
+	})
+	b.Run("primitive", func(b *testing.B) {
+		benchmarkRefCounter(b, New(), stackitem.NewBigInteger(big.NewInt(12345678)))
+		b.Run("big stack", func(b *testing.B) {
+			v := New()
+			for i := 0; i < 100; i++ {
+				v.estack.PushVal(stackitem.NewBigInteger(big.NewInt(13)))
+			}
+			benchmarkRefCounter(b, v, stackitem.NewBigInteger(big.NewInt(12345678)))
+		})
+	})
+	b.Run("put map on a big stack", func(b *testing.B) {
+		v := New()
+		for i := 0; i < 100; i++ {
+			v.estack.PushVal([]stackitem.Item{stackitem.NewBool(true)})
+		}
+
+		m := stackitem.NewMap()
+		m.Add(stackitem.Make(1), stackitem.Make(2))
+		m.Add(stackitem.Make(2), stackitem.Make([]stackitem.Item{}))
+		m.Add(stackitem.Make(3), stackitem.Make([]stackitem.Item{stackitem.NewBool(false)}))
+	})
+}
+
+func benchmarkRefCounter(b *testing.B, v *VM, item stackitem.Item) {
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		v.estack.PushVal(item)
+		v.estack.Pop()
+	}
 }
